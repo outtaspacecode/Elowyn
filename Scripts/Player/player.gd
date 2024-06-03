dfd22ddffbdbf1b14fb612ec1c52098b931914a8
colebeebe <cole.beebe@protonmail.com>
2024-06-02 19:11:31 -0600

    Reworked some animations

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 
 @export var speed : int = 60
 @onready var animation = $AnimationPlayer
+var direction = ""
 
 func _physics_process(_delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 	move_and_slide()
 	animate()
 
@@ -15,9 +16,13 @@ func animate():
 			animation.stop()
 		return
 	
-	var direction = "down"
-	#if velocity.x < 0: direction = "left"
-	#elif velocity. x > 0: direction = "right"
-	#elif velocity.y < 0: direction = "up"
 	
-	animation.play("walk_" + direction)
+	if velocity.x < 0 && velocity.y == 0: direction = "left"
+	elif velocity. x > 0 && velocity.y == 0: direction = "right"
+	elif velocity.y < 0 && velocity.x == 0: direction = "up"
+	elif velocity.y > 0 && velocity.x == 0: direction = "down"
+	
+	var speed = "walk_"
+	
+	if direction != "":
+		animation.play(speed + direction)
