bb3c47cfaf3e1531fd07e6f1c8e579b6e41b005f
colebeebe <cole.beebe@protonmail.com>
2024-06-04 00:30:15 -0600

    Camera is properly implemented
    A few bugs still exist here and there, but for the most part, the camera is well implemented, and with careful consideration, any bugs will not be noticed

--- Scripts/Player/camera_controller.gd
+++ Scripts/Player/camera_controller.gd
@@ -1,22 +1,32 @@
 extends Camera2D
 
+var resolution : Vector2 = Vector2(480, 264)
+
 func _ready():
 	Events.room_entered.connect(func(room):
+		var player = get_parent()
 		var tween = create_tween()
 		get_tree().paused = true
 		reset_limits()
-		tween.tween_property(self, "global_position", room.global_position + Vector2(240, 132), 0.5)
+		var coming_from : Vector2 = floor(player.global_position / resolution)
+		global_position = centerize(coming_from * resolution)
+		var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
+		var going_to : Vector2 = coming_from + direction
+		tween.tween_property(self, "global_position", centerize(going_to * resolution), 0.75)
+		player.global_position += direction * Vector2(10, 10)
 		
 		tween.finished.connect(func():
 			get_tree().paused = false
-			print("here")
 			limit_bottom = room.global_position.y + room.screens.y * 264
 			limit_top = room.global_position.y
 			limit_left = room.global_position.x
 			limit_right = room.global_position.x + room.screens.x * 480
-			global_position = get_parent().global_position
+			global_position = player.global_position
 		)
 	)
+
+func centerize(pos : Vector2) -> Vector2:
+	return pos + (resolution / Vector2(2, 2))
 
 func reset_limits() -> void:
 	limit_left = -10000000
