05c7baf750f82c84797e0245fb286e8952ddeb0d
colebeebe <cole.beebe@protonmail.com>
2024-06-05 03:57:01 -0600

    Began reworking cam controls (again)

--- Scripts/Player/camera_controller.gd
+++ Scripts/Player/camera_controller.gd
@@ -4,23 +4,20 @@ var resolution : Vector2 = Vector2(480, 264)
 
 func _ready():
 	Events.room_entered.connect(func(room):
-		var player = get_parent()
+		var player = %player
 		var tween = create_tween()
 		get_tree().paused = true
 		reset_limits()
-		var coming_from : Vector2 = floor(player.global_position / resolution)
-		global_position = centerize(coming_from * resolution)
-		var going_to : Vector2 = coming_from + player.facing_direction
-		tween.tween_property(self, "global_position", centerize(going_to * resolution), 0.75)
+		tween.tween_property(self, "global_position", room.global_position, 0.75)
 		player.global_position += player.facing_direction * Vector2(10, 10)
 		
 		tween.finished.connect(func():
 			get_tree().paused = false
-			limit_bottom = room.global_position.y + room.screens.y * 264
-			limit_top = room.global_position.y
-			limit_left = room.global_position.x
-			limit_right = room.global_position.x + room.screens.x * 480
-			global_position = player.global_position
+			#limit_bottom = room.global_position.y + room.screens.y * 264
+			#limit_top = room.global_position.y
+			#limit_left = room.global_position.x
+			#limit_right = room.global_position.x + room.screens.x * 480
+			#global_position = player.global_position
 		)
 	)
 
--- Scripts/Player/player_detector.gd
+++ Scripts/Player/player_detector.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
-@export var screens : Vector2i = Vector2i(1, 1)
-
-func _ready():
-	scale.x = screens.x * 24
-	scale.y = screens.y * 13.2
+@export var screens : Vector4 = Vector4()
 
 func _on_body_entered(_body):
 	Events.room_entered.emit(self)
